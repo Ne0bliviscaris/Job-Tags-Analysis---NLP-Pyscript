c7cc4dfad340dfa3ac7f116bb0cb016bf44ef442
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2025-05-16 14:38:39 +0200

    feature: app loading spinner

--- scripts/main.py
+++ scripts/main.py
@@ -1,1 +1,19 @@
-print("Hello from Python!")
+from js import document
+
+from pyscript import display
+
+
+def clear_output():
+    """Clears the output container"""
+    output_element = document.getElementById("output")
+    output_element.innerHTML = ""
+
+
+def show_content():
+    """Display message on the page"""
+    return "Hello from Python! PyScript working properly."
+
+
+clear_output()
+# message = show_content()
+# display(message, target="output")
