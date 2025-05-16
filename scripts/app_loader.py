35c996cada0c9e1460ae82e6a08ad46ef4a5198c
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2025-05-16 17:42:24 +0200

    feature: load CSV data and display in main app

--- scripts/app_loader.py
+++ scripts/app_loader.py
@@ -1,5 +1,10 @@
-from js import document
+from io import StringIO
 
+import pandas as pd
+from js import document
+from pyodide.http import pyfetch
+
+import pyscript
 from pyscript import display
 
 block_id = "app_loader"
@@ -12,11 +17,25 @@ def clear_loader():
     return loading_animation
 
 
-def load_app():
+async def load_csv():
+    """Load CSV from URL and return DataFrame."""
+    response = await pyfetch(
+        "https://raw.githubusercontent.com/Ne0bliviscaris/Job-Tags-Analysis---NLP-Pyscript/main/data/job%20offers%20tags.csv"
+    )
+    text = await response.string()
+    return pd.read_csv(StringIO(text))
+
+
+async def main_app():
+    """Main app logic."""
+    data = await load_csv()
+    display(data)
+
+
+async def load_app():
     """Ładuje aplikację zachowując strukturę kontenera"""
-    container = clear_loader()
-    welcome_message = "Hello from Python! PyScript working properly."
-    container.innerHTML = welcome_message
+    clear_loader()
+    await main_app()
 
 
 load_app()
